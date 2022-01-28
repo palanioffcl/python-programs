e2414149aa11e22cef2ff198be7c9e9ab6654ff0
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-01-28 12:02:15 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,15 @@
+# Decreasing Triangle Pattern
+
+num = int(input("Enter your Desired size of pattern you need = "))
+
+for i in range(num,0,-1):
+  print('*'*i)
+  
+  
+  
+  #increasing Triangle Pattern
+  
+  num = int(input("Enter your Desired size of pattern you need = "))
+
+for i in range(num,0,1):
+  print('*'*i)
